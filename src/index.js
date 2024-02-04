23fa8bc6c79bd614096a20377c91e176712eb3da
ShreeshHunnur <shreesh1060@gmail.com>
2024-02-04 23:10:06 +0530

    more basic configs

--- src/index.js
+++ src/index.js
@@ -6,41 +6,16 @@ dotenv.config({
 })
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-( async() => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-        app.on("error", (error) =>{
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT , () => {
-            console.log(`App is Listening on port ${process.env.PORT}`);
-        })
-        
-    } catch (error) {
-        console.error("ERROR: ",error);
+.then( () => {
+    app.on("error", (error) => {
+        console.log("ERRR: ",error);
         throw error
-    }
-})()
-*/
+    })
+
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`The Server is running on port ${process.env.PORT} `);
+    })
+})
+.catch( (err) => {
+    console.log("MongoDB connection error: ",err)
+})
