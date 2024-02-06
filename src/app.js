39027f969ab56570736272aa4daac9d13551d097
ShreeshHunnur <shreesh1060@gmail.com>
2024-02-07 00:32:24 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+
+const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -12,6 +14,6 @@ app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser())
 
-const app = express()
+
 
 export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,45 @@
+const asyncHandler = (requestHandler) => {
+    (req ,res,next) => {
+        Promise.resolve(requestHandler(req , res , next)).catch((error) => next(error))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//One another method to write asyncHandler using Try Catch- Both can be used
+
+// const asyncHandler  = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req , res, next)
+//     } catch (error) {
+//         res.error(error.code || 500).json({
+//             success:true,
+//             message: error.message
+//         })
+//     }
+// }
